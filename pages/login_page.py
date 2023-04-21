443ac3a09e337822dec33f66afee01401a01d5ab
Keeper <virys2@gmail.com>
2023-04-21 13:07:34 +0300

    Создание метода для получения нынешней URL

--- pages/login_page.py
+++ pages/login_page.py
@@ -48,6 +48,7 @@ class LoginPage(Base):
     def authorization(self):
         self.driver.get(self.url)
         self.driver.maximize_window()
+        self.get_current_url()
         self.input_user_name("standard_user")
         self.input_password("secret_sauce")
         self.click_login_button()
