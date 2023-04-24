c9612865b21eda0ddf3f44c5086c9e80cff43288
Keeper <virys2@gmail.com>
2023-04-24 10:02:49 +0300

    Создание метода для проверки значения текста на странице

--- base/base_class.py
+++ base/base_class.py
@@ -8,3 +8,9 @@ class Base():
     def get_current_url(self):
         get_url = self.driver.current_url
         print("Current url " + get_url)
+
+    # Method assert word
+    def assert_word(self, word, result):
+        value_word = word.text
+        assert value_word == result
+        print("Good value word")
--- pages/login_page.py
+++ pages/login_page.py
@@ -7,16 +7,16 @@ class LoginPage(Base):
 
     url = "https://www.saucedemo.com/"
 
-    def __init__(self, driver):
-        super().__init__(driver)
-        self.driver = driver
+    # def __init__(self, driver):
+    #     super().__init__(driver)
+    #     self.driver = driver
 
     # Locators
     user_name = 'user-name'
     password = 'password'
     login_button = 'login-button'
-
-
+    main_word = '//span[@class="title"]'
+    
     # Getters
     def get_user_name(self):
         return  WebDriverWait(self.driver, 30)\
@@ -29,6 +29,10 @@ class LoginPage(Base):
     def get_login_button(self):
         return WebDriverWait(self.driver, 30)\
             .until(EC.element_to_be_clickable((By.ID, self.login_button)))
+    
+    def get_main_word(self):
+        return WebDriverWait(self.driver, 30)\
+            .until(EC.element_to_be_clickable((By.XPATH, self.main_word)))
 
     # Actions
     def input_user_name(self, user_name: str):
@@ -52,3 +56,4 @@ class LoginPage(Base):
         self.input_user_name("standard_user")
         self.input_password("secret_sauce")
         self.click_login_button()
+        self.assert_word(self.get_main_word(), 'Products')
