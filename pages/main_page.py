486d54617655ef64ef3bdd60d1a0d79f2471206b
Keeper <virys2@gmail.com>
2023-04-27 09:45:23 +0300

    Создание нескольких тестовых методов в одном файле

--- pages/main_page.py
+++ pages/main_page.py
@@ -6,7 +6,9 @@ from selenium.webdriver.common.by import By
 class MainPage(Base):
     
     # Locators
-    select_product_1 = '//button[@id="add-to-cart-sauce-labs-backpack"]'
+    product_1 = '//button[@id="add-to-cart-sauce-labs-backpack"]'
+    product_2 = '//button[@id="add-to-cart-sauce-labs-bike-light"]'
+    product_3 = '//button[@id="add-to-cart-sauce-labs-bolt-t-shirt"]'
     cart = '//div[@id="shopping_cart_container"]'
     menu_btn = '//button[@id="react-burger-menu-btn"]'
     about_link = '//a[@id="about_sidebar_link"]'
@@ -14,7 +16,15 @@ class MainPage(Base):
     # Getters
     def get_product_1(self):
         return WebDriverWait(self.driver, 30)\
-            .until(EC.element_to_be_clickable((By.XPATH, self.select_product_1)))
+            .until(EC.element_to_be_clickable((By.XPATH, self.product_1)))
+    
+    def get_product_2(self):
+        return WebDriverWait(self.driver, 30)\
+            .until(EC.element_to_be_clickable((By.XPATH, self.product_2)))
+    
+    def get_product_3(self):
+        return WebDriverWait(self.driver, 30)\
+            .until(EC.element_to_be_clickable((By.XPATH, self.product_3)))
     
     def get_cart(self):
         return WebDriverWait(self.driver, 30)\
@@ -33,6 +43,14 @@ class MainPage(Base):
         self.get_product_1().click()
         print("Click select product 1")
 
+    def click_select_product_2(self):
+        self.get_product_2().click()
+        print("Click select product 1")
+
+    def click_select_product_3(self):
+        self.get_product_3().click()
+        print("Click select product 1")
+
     def click_cart(self):
         self.get_cart().click()
         print("Enter the cart")
@@ -45,9 +63,19 @@ class MainPage(Base):
         self.get_about_link().click()
 
     # Methods
-    def select_product(self):
+    def select_product_1(self):
         self.get_current_url()
         self.click_select_product_1()
+        self.click_cart()
+
+    def select_product_2(self):
+        self.get_current_url()
+        self.click_select_product_2()
+        self.click_cart()
+
+    def select_product_3(self):
+        self.get_current_url()
+        self.click_select_product_3()
         self.click_cart()
 
     def select_menu_about(self):
---
+++ tests/test_buy_product_1.py
@@ -0,0 +1,68 @@
+import sys; sys.path.append('../automation_project_1')
+from pages.client_information_page import ClientInformationPage
+from pages.cart_page import CartPage
+from pages.login_page import LoginPage
+from pages.main_page import MainPage
+from pages.payment_page import PaymentPage
+from pages.finish_page import Finish_page
+import time, pytest
+
+@pytest.mark.run(order=3)
+def test_buy_product_1(driver):
+    print("Start test 1")
+    
+    login = LoginPage(driver)
+    login.authorization()
+    time.sleep(1.5)
+    
+    mp = MainPage(driver)
+    mp.select_product_1()
+    time.sleep(1.5)
+
+    cp = CartPage(driver)
+    cp.click_checkout_button()
+    time.sleep(1.5)
+
+    # cip = ClientInformationPage(driver)
+    # cip.input_information()
+    # time.sleep(1.5)
+
+    # p = PaymentPage(driver)
+    # p.click_finish_btn()
+    # time.sleep(1.5)
+
+    # f = Finish_page(driver)
+    # f.finish()
+    # time.sleep(1.5)
+
+@pytest.mark.run(order=1)
+def test_buy_product_2(driver):
+    print("Start test 2")
+    
+    login = LoginPage(driver)
+    login.authorization()
+    time.sleep(1.5)
+    
+    mp = MainPage(driver)
+    mp.select_product_2()
+    time.sleep(1.5)
+
+    cp = CartPage(driver)
+    cp.click_checkout_button()
+    time.sleep(1.5)
+
+@pytest.mark.run(order=2)
+def test_buy_product_3(driver):
+    print("Start test 3")
+    
+    login = LoginPage(driver)
+    login.authorization()
+    time.sleep(1.5)
+    
+    mp = MainPage(driver)
+    mp.select_product_3()
+    time.sleep(1.5)
+
+    cp = CartPage(driver)
+    cp.click_checkout_button()
+    time.sleep(1.5)
